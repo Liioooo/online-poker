c63f3988a44fba64662439c4be8b5e5247f181dc
Huldula <38780229+Huldula@users.noreply.github.com>
2020-04-29 13:06:40 +0200

    Add most basic card comparison functions for Cards

--- server/models/cards.ts
+++ server/models/cards.ts
@@ -4,7 +4,7 @@ export type playingCard = string; // length of 2: first is suit, second is type 
 
 export class Cards {
 
-	public static suits = ['H', 'T', 'C', 'S']; // hearth, tile, club, spade
+	public static suits = ['H', 'T', 'C', 'S']; // heart, tile, club, spade
 	public static numbers = ['2', '3', '4', '5', '6', '7', '8', '9', '0', 'J', 'Q', 'K', 'A'];
 
 	public static isAce(card: playingCard): boolean {
@@ -28,5 +28,13 @@ export class Cards {
 
 	public static newDeck(): playingCard[] {
 		return Cards.shuffle(Cards.sortedDeck());
+	}
+
+	public static hasSameSuit(cardA: playingCard, cardB: playingCard): boolean {
+		return cardA[0] === cardB[0];
+	}
+
+	public static hasSameNumber(cardA: playingCard, cardB: playingCard): boolean {
+		return cardA[1] === cardB[1];
 	}
 }
